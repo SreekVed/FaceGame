a49c721deb776a361b9c6dbf4f3bed1f18e7f4bd
Sreekar Vedula <sreekar.vedula@gmail.com>
2021-05-06 18:23:07 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends React.Component {
     this.setState({ playing: false, end: true, checked: '', images: '', diff: ''});
     document.getElementById("balance").style.color = this.state.balance >= 100 ? "lime" : "red";
 
-    fetch("/api/data", {
+    fetch("/data", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
